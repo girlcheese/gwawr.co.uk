d8bd9afa66370edb00a24dc68c63d2d1ba92aaaa
Sam Carrington <github@gwawr.co.uk>
2018-09-03 12:20:19 +0100

    checkstyle and lint fixing - add prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,22 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'gwawr.co.uk',
+    title: "gwawr.co.uk",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Gwawr portfolio site using NuxtJS' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Gwawr portfolio site using NuxtJS"
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -24,16 +26,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
         })
       }
     }
   }
 }
-
