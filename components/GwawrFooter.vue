1f0b29b896837919d4a13fa65ed4cc45dc2f28fb
Sam Carrington <sam.carrington@analogfolk.com>
2021-05-10 13:42:21 +0100

    add footer

---
+++ components/GwawrFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer
+    class="bg-gray-400 w-full p-5 border-t border-gray-300 fixed bottom-0"
+  >
+    <div class="container flex items-center justify-between px-6 mx-auto">
+      <p class="text-gray-500">(c) 2021 All rights reserved</p>
+      <p class="font-medium text-gray-700">Terms of Service</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'GwawrFooter',
+}
+</script>
+
+<style scoped></style>
